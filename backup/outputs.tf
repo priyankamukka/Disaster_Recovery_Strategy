acf0223c1063a0cbd52f7c2dfcadef21986e6a26
priyankamukka <mukkapriyanka903@gmail.com>
2024-10-17 22:22:46 +0530

    backup changes

--- backup/outputs.tf
+++ backup/outputs.tf
@@ -1,23 +1,7 @@
-# Output for Backup Plan ARN
-output "backup_plan_arn" {
-  description = "The ARN of the backup plan"
-  value       = aws_backup_plan.backup_plan.arn
-}
-
-# Output for Backup Plan ID
-output "backup_plan_id" {
-  description = "The ID of the backup plan"
-  value       = aws_backup_plan.backup_plan.id
-}
-
-# Output for Daily Backup Rule Name
 output "daily_backup_rule_name" {
-  description = "The name of the daily backup rule"
-  value       = aws_backup_plan.backup_plan.rule[0].rule_name
+  value = [for rule in aws_backup_plan.backup_plan.rule : rule.rule_name if rule.rule_name != null][0]
 }
 
-# Output for General Backup Rule Name
 output "backup_rule_name" {
-  description = "The name of the general backup rule"
-  value       = aws_backup_plan.backup_plan.rule[1].rule_name
+  value = [for rule in aws_backup_plan.backup_plan.rule : rule.rule_name if rule.rule_name != null][1]
 }
